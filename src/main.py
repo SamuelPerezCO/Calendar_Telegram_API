0179cfaebce258f62b56761a140c625b23300540
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-07-14 11:46:50 -0500

    handler for the first message and import from the controller

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 """
 t.me/Calendar_Telegram12_Bot.
 """
-from telegram.ext import ApplicationBuilder
+from telegram.ext import ApplicationBuilder , CommandHandler
 from telegram import Update
 from dotenv import load_dotenv
 import os
+
+from Controllers.calendarController import calendarController
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 application = ApplicationBuilder().token(BOT_TOKEN).build()
+application.add_handler( CommandHandler("start" , calendarController.show_menu))
+
 
 application.run_polling(allowed_updates=Update.ALL_TYPES)
