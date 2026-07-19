1c3fdcf32be68154696e31191c3c86a5267a1759
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-07-19 12:12:50 -0500

    Handlers to get the time and date

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 """
 t.me/Calendar_Telegram12_Bot.
 """
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, CommandHandler, ConversationHandler, MessageHandler, filters
 
 from telegram import Update
 from dotenv import load_dotenv
 import os
 
-from Controllers.calendarController import calendarController
+from Controllers.calendarController import calendarController, TITLE, DATE, TIME
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -16,5 +16,16 @@ application = ApplicationBuilder().token(BOT_TOKEN).build()
 application.add_handler( CommandHandler("start" , calendarController.show_menu))
 
 application.add_handler(MessageHandler(filters.Regex("^Create Task or Event$"), calendarController.create_task_or_event))
+
+create_event_conversation = ConversationHandler(
+    entry_points=[MessageHandler(filters.Regex("^Create a Event$"), calendarController.create_event_start)],
+    states={
+        TITLE: [MessageHandler(filters.TEXT & ~filters.COMMAND, calendarController.create_event_title)],
+        DATE: [MessageHandler(filters.TEXT & ~filters.COMMAND, calendarController.create_event_date)],
+        TIME: [MessageHandler(filters.TEXT & ~filters.COMMAND, calendarController.create_event_time)],
+    },
+    fallbacks=[CommandHandler("cancel", calendarController.cancel)],
+)
+application.add_handler(create_event_conversation)
 
 application.run_polling(allowed_updates=Update.ALL_TYPES)
