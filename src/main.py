2e682c152e4e832a4bf9e5a0da70903505623c98
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-07-19 11:42:58 -0500

    handler for creating a task or event

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 """
 t.me/Calendar_Telegram12_Bot.
 """
-from telegram.ext import ApplicationBuilder , CommandHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+
 from telegram import Update
 from dotenv import load_dotenv
 import os
@@ -14,5 +15,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 application = ApplicationBuilder().token(BOT_TOKEN).build()
 application.add_handler( CommandHandler("start" , calendarController.show_menu))
 
+application.add_handler(MessageHandler(filters.Regex("^Create Task or Event$"), calendarController.create_task_or_event))
 
 application.run_polling(allowed_updates=Update.ALL_TYPES)
